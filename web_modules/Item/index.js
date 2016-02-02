18e25ba471f25edbf467b2939dd1daa49f335ed7
lealivran <lealivran@msn.com>
2016-02-02 19:11:12 +0100

    list artist

---
+++ web_modules/Item/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Item = ({name = ""}) => {
+	return <span className="item">{name}</span>
+}
+
+export default Item
--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 
+import Item from "Item"
+import List from "List"
+
+const artists = [
+	{name:"Bob Marley"},
+	{name:"Bob Dylan"},
+	{name:"Muse"},
+	{name:"Lana Del Rey"}
+]
+const kinds = {
+	rap: {name:"Rap"},
+	rock: {name:"Rock"},
+	electro: {name:"Electro"}
+}
+
+let	kindArray = Object.keys(kinds)
+console.log(kindArray)
+
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello React !
-      </div>
-    );
-  }
+
+	getItem(artist, index){
+		return (<Item key={index} name={artist.name}/>)
+	}
+	render() {
+		return (
+			<div>
+			<List items={artists} />
+			{
+				kinds &&
+				kindArray.map((key, index) => {
+					console.log(kindArray[key])
+				})
+			}
+			</div>
+		);
+	}
 }
