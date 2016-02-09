a0c10ec42e16d35d6e00aba391a28c47c39c017f
lealivran <lealivran@msn.com>
2016-02-09 14:57:54 +0100

    add http request

--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import List from "List";
 
-const artists = [
-  {name:"Bob Marley"},
-  {name:"Bob Dylan"},
-  {name:"Odezenne"},
-  {name:"Kavinsky"}
-]
+// const artists = [
+//   {name:"Bob Marley"},
+//   {name:"Bob Dylan"},
+//   {name:"Odezenne"},
+//   {name:"Kavinsky"}
+// ]
 
 const kinds = {
   rap: {name:"Rap"},
@@ -17,10 +17,29 @@ const kinds = {
 const kindsArray = Object.keys(kinds).map((k) => { return kinds[k] });
 
 export default class App extends Component {
+
+  state = {
+    artists:[]
+  };
+
+  componentDidMount(){
+    console.log("")
+
+    const url = 'https://api.spotify.com/v1/search?q=tania*&type=artist';
+    fetch(url).then((response) => {
+      response.json().then((data) => {
+        if (!data.error) {
+          console.log(data)
+          this.setState({artists: data.artists.items})
+        };
+      })
+    })
+
+  }
   render() {
     return (
       <div>
-        <List title="Artist" items={artists} />
+        <List title="Artist" items={this.state.artists} />
         <List title="Kind" items={kindsArray} />
       </div>
     )
